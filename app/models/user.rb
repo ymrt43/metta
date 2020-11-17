6275e2234ba8747fd241013f9a53a3cf2bec1f7a
ymrt43 <ymrt43@gmail.com>
2020-11-17 22:19:01 +0900

    Add associations into User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :profile
+  has_one :card
+  has_many :sns_credentials
+  has_many :reservations
+  has_many :lessons, through: :reservations
 end
