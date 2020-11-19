9ccfe504510a93cad244da3704f230d182b41e72
ymrt43 <ymrt43@gmail.com>
2020-11-20 03:12:35 +0900

    Add routings for registrations&users#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'profiles', to: 'users/registrations#new_profile'
+    post 'profiles', to: 'users/registrations#create_profile'
+  end
+  resources :users, only: :new
   root to: 'lessons#index'
 end
