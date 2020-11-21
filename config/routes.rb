dee91c7d445d5e209c16633aceef124145a2c6c3
ymrt43 <ymrt43@gmail.com>
2020-11-21 18:06:05 +0900

    Create controller & Add route for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
   }
   devise_scope :user do
     get 'profiles', to: 'users/registrations#new_profile'
