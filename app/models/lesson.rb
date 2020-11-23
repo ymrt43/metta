acc2c898804039ac38ab3bcb0f0377f0830d4db4
ymrt43 <ymrt43@gmail.com>
2020-11-24 00:02:22 +0900

    Delete unnecessary :

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,5 +2,5 @@ class Lesson < ApplicationRecord
   has_many :reservations
   belongs_to :course
 
-  validates: :level, :capacity, :date, :start_time, :end_time, :fee, presence: true
+  validates :level, :capacity, :date, :start_time, :end_time, :fee, presence: true
 end
