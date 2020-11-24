79898fe0a44a11d44c397e82359d7d02228e132d
ymrt43 <ymrt43@gmail.com>
2020-11-24 23:40:24 +0900

    Create lessons#index,new,create,edit,update&destroy function

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,61 @@
 class LessonsController < ApplicationController
+  before_action :set_lesson, only: [:edit, :update, :show, :destroy]
+
   def index
+    @lessons = Lesson.all
   end
+
+  def new
+    @lesson = Lesson.new
+    @courses = Course.all
+  end
+  
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      redirect_to lessons_path, notice: 'レッスン予定を公開しました'
+    else
+      flash.now[:alert] = 'レッスン予定の公開に失敗しました'
+      render :new
+    end
+  end
+  
+  def edit
+    @courses = Course.all
+  end
+
+  def update
+    if @lesson.update(lesson_params)
+      redirect_to lessons_path, notice: 'レッスン予定を変更しました'
+    else
+      flash.now[:alert] = 'レッスン更新に失敗しました'
+      render :edit
+    end
+  end
+
+  def destroy
+    if @lesson.destroy
+      redirect_to lessons_path, notice: 'レッスン予定を削除しました'
+    else
+      flash.now[:alert] = '削除に失敗しました'
+      render :index
+    end
+  end
+
+  def home
+  end
+
+  def adminuser
+  end
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:level, :capacity, :date, :start_time, :end_time, :fee, :lesson_introduction, :course_id, :fully_booked) 
+  end
+
+  def set_lesson
+    @lesson = Lesson.find(params[:id])
+  end
+
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,4 +3,6 @@ class Lesson < ApplicationRecord
   belongs_to :course
 
   validates :level, :capacity, :date, :start_time, :end_time, :fee, presence: true
+
+  enum level: { "初心者向け": 1, "中級者向け": 2, "上級者向け": 3 }
 end
