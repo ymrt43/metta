88791e829663c88448ad59f4e8e94f4a536c0a04
ymrt43 <ymrt43@gmail.com>
2020-11-29 21:56:13 +0900

    Update image_uploader (large_of_resize)

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -45,6 +45,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
-  process resize_to_fit: [100, 100]
+  process resize_to_fit: [500, 500]
   
 end
