506a5674f71b4cdd823aaa8c003fee14cbee309a
ymrt43 <ymrt43@gmail.com>
2020-11-29 21:59:11 +0900

    Fix spell mistakes in reservations_controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,12 +1,15 @@
 class ReservationsController < ApplicationController
+  before_action :set_lesson
+
   def new
     @reservation = Reservation.new
+    @lesson_duration = @lesson.lesson_duration
   end
 
   def create
-    @reservation = Reservation.new(reservation_params)
+    @reservation = @lesson.reservations.create(reservation_params)
     unless @reservation.save
-      flash.now[:alert] = '申し訳ございません お電話にてお問い合わせください'
+      flash.now[:alert] = '申し訳ございません 再度送信いただくか、お電話にてお問い合わせください'
       render :new
     end
   end
@@ -14,6 +17,10 @@ class ReservationsController < ApplicationController
   private
 
   def reservation_params
-    params.require(:reservation).permit(:count, :lesson_id).marge(user_id: current_user.id)
+    params.require(:reservation).permit(:count).merge(user_id: current_user.id)
+  end
+
+  def set_lesson
+    @lesson = Lesson.find(params[:lesson_id])
   end
 end
