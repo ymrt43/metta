c996ba6b56e5c09a993fca12af31f99bcad061f9
ymrt43 <ymrt43@gmail.com>
2020-12-09 06:01:49 +0900

    Add definition participants

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,9 @@ class Reservation < ApplicationRecord
   belongs_to :user
 
   validates :count, presence: true
+
+  def number_of_participants(lesson_id)
+    participants = Reservation.where(lesson_id: lesson_id).sum("count")
+    return participants
+  end
 end
